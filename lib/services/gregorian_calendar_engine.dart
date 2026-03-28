d5d11eb37c9948e97b010cf9db4c0e5e8964b173
SisekoBunu <mrsisekobunu@gmail.com>
2026-03-28 19:00:48 +0200

    Add Easter calculation (Good Friday, Easter Sunday, Easter Monday) and finalize Gregorian calendar

--- lib/services/gregorian_calendar_engine.dart
+++ lib/services/gregorian_calendar_engine.dart
@@ -167,6 +167,44 @@ class GregorianCalendarEngine implements CalendarEngine {
     holidays.add("Black Friday");
   }
 
+  
+  // Easter calculation (Anonymous Gregorian algorithm)
+  DateTime getEaster(int year) {
+    final a = year % 19;
+    final b = year ~/ 100;
+    final c = year % 100;
+    final d = b ~/ 4;
+    final e = b % 4;
+    final f = (b + 8) ~/ 25;
+    final g = (b - f + 1) ~/ 3;
+    final h = (19 * a + b - d - g + 15) % 30;
+    final i = c ~/ 4;
+    final k = c % 4;
+    final l = (32 + 2 * e + 2 * i - h - k) % 7;
+    final m = (a + 11 * h + 22 * l) ~/ 451;
+    final month = (h + l - 7 * m + 114) ~/ 31;
+    final day = ((h + l - 7 * m + 114) % 31) + 1;
+
+    return DateTime(year, month, day);
+  }
+
+  final easter = getEaster(year);
+
+  final goodFriday = easter.subtract(const Duration(days: 2));
+  final easterMonday = easter.add(const Duration(days: 1));
+
+  if (monthIndex == easter.month - 1 && day == easter.day) {
+    holidays.add("Easter Sunday");
+  }
+
+  if (monthIndex == goodFriday.month - 1 && day == goodFriday.day) {
+    holidays.add("Good Friday");
+  }
+
+  if (monthIndex == easterMonday.month - 1 && day == easterMonday.day) {
+    holidays.add("Easter Monday");
+  }
+
   return holidays;
 }
 
@@ -179,6 +217,7 @@ class GregorianCalendarEngine implements CalendarEngine {
     return const [];
   }
 }
+
 
 
 
